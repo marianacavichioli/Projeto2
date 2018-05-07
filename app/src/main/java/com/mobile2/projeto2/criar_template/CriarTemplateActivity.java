7bc11f61261321db6b80a3439773071dff7693d8
ylovalho <yuri.lovalho@gmail.com>
2018-05-07 10:57:45 -0300

    [atividade-fotos] layout do Template para criação da atividade e validação de preenchimento dos campos

--- app/src/main/java/com/mobile2/projeto2/criar_template/CriarTemplateActivity.java
+++ app/src/main/java/com/mobile2/projeto2/criar_template/CriarTemplateActivity.java
@@ -10,11 +10,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.mobile2.projeto2.MainActivity;
 import com.mobile2.projeto2.R;
 import com.squareup.picasso.Picasso;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -29,8 +33,14 @@ public class CriarTemplateActivity extends AppCompatActivity implements CriarTem
 
     private CriarTemplatePresenter presenter;
 
+    private String palavra, silabaCorreta, silabaIncorreta1, silabaIncorreta2;
+    public EditText editText_Palavra, editText_SilabaCorreta, editText_SilabaIncorreta1, editText_SilabaIncorreta2;
+
+
+
     private static final int CODIGO_CAMERA = 123;
     public String caminhoFoto;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +49,11 @@ public class CriarTemplateActivity extends AppCompatActivity implements CriarTem
 
         ButterKnife.bind(this);
         presenter = new CriarTemplatePresenter(this);
+
+        editText_Palavra = findViewById(R.id.editText_Palavra);
+        editText_SilabaCorreta = findViewById(R.id.editText_SilabaCorreta);
+        editText_SilabaIncorreta1 = findViewById(R.id.editText_SilabaIncorreta1);
+        editText_SilabaIncorreta2 = findViewById(R.id.editText_SilabaIncorreta2);
 
     }
 
@@ -75,12 +90,57 @@ public class CriarTemplateActivity extends AppCompatActivity implements CriarTem
 
     @OnClick(R.id.formulario_submit)
     public void salvar(){
-        Intent resultado = new Intent().putExtra("template",
-                presenter.getTemplate(caminhoFoto));
 
-        setResult(Activity.RESULT_OK, resultado);
-        finish();
+        palavra = editText_Palavra.getText().toString();
+        silabaCorreta = editText_SilabaCorreta.getText().toString();
+        silabaIncorreta1 = editText_SilabaIncorreta1.getText().toString();
+        silabaIncorreta2 = editText_SilabaIncorreta2.getText().toString();
 
+        List<String> mensagens = validar();
+
+        if (mensagens == null) {
+            Intent resultado = new Intent().putExtra("template",
+                    presenter.getTemplate(caminhoFoto));
+
+            setResult(Activity.RESULT_OK, resultado);
+
+
+
+
+            showToast(palavra);
+            finish();
+        }
+
+        else{
+            showErro(mensagens);
+        }
+
+    }
+
+    private void showToast(String text){
+        Toast.makeText(CriarTemplateActivity.this, text, Toast.LENGTH_SHORT).show();
+    }
+
+    private void showErro(List<String> erro) {
+        Toast.makeText(CriarTemplateActivity.this, erro.get(0), Toast.LENGTH_SHORT).show();
+    }
+
+    public List<String> validar(){
+        List<String> mensagens = new ArrayList<String>();
+        if (palavra.trim().length() == 0) {
+            mensagens.add("O primeiro campo deve ser preenchido");
+        }
+        if (silabaCorreta.trim().length() == 0) {
+            mensagens.add("O segundo campo deve ser preenchido");
+        }
+        if (silabaIncorreta1.trim().length() == 0) {
+            mensagens.add("O terceiro campo deve ser preenchido");
+        }
+        if (silabaIncorreta2.trim().length() == 0) {
+            mensagens.add("O quarto campo deve ser preenchido");
+        }
+
+        return (mensagens.isEmpty() ? null : mensagens);
     }
 
 }
