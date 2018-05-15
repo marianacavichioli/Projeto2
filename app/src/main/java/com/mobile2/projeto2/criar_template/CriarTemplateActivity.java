b30a90279f25bb9edfafcf5c1001ba879baf85d4
Renata <renatassmendes@hotmail.com>
2018-05-14 21:44:51 -0300

    Colocando foto da galeria! Porem sem salvar caminho foto

--- app/src/main/java/com/mobile2/projeto2/criar_template/CriarTemplateActivity.java
+++ app/src/main/java/com/mobile2/projeto2/criar_template/CriarTemplateActivity.java
@@ -1,10 +1,17 @@
 package com.mobile2.projeto2.criar_template;
 
+import android.Manifest;
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.annotation.RequiresApi;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -13,13 +20,12 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import android.database.Cursor;
-import android.database.DatabaseUtils;
 
-import com.mobile2.projeto2.MainActivity;
 import com.mobile2.projeto2.R;
 import com.squareup.picasso.Picasso;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -57,6 +63,8 @@ public class CriarTemplateActivity extends AppCompatActivity implements CriarTem
         editText_Palavra = findViewById(R.id.editText_Palavra);
     }
 
+    @TargetApi(Build.VERSION_CODES.M)
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == CODIGO_CAMERA && resultCode == Activity.RESULT_OK) {
@@ -65,23 +73,66 @@ public class CriarTemplateActivity extends AppCompatActivity implements CriarTem
         }
         else if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
 
-//            fotoAnexada = true;
-//
-//            Uri uri = data.getData();
-//            String[] projection = { MediaStore.Images.Media.DATA };
-//
-//            Cursor cursor = getContentResolver().query(uri, projection, null, null, null);
-//            cursor.moveToFirst();
-//
-//            Log.d("TAG", DatabaseUtils.dumpCursorToString(cursor));
-//
-//            int columnIndex = cursor.getColumnIndex(projection[0]);
-//            caminhoFoto = cursor.getString(columnIndex); // returns null
-//            cursor.close();
-//
-//            exibeFoto();
+            fotoAnexada = true;
+
+            /*
+            // se necessario mais permissao
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+                // Should we show an explanation?
+                if (shouldShowRequestPermissionRationale(
+                        Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                    // Explain to the user why we need to read the contacts
+                }
+
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        PICK_IMAGE_REQUEST);
+
+            */ // colocar fecha } no final
+
+            /*
+            // Retorna nulo o caminho
+                Uri uri = data.getData();
+                String[] projection = { MediaStore.Images.Media.DATA };
+
+
+                Cursor cursor = getContentResolver().query(uri, projection, null, null, null);
+                cursor.moveToFirst();
+
+                Log.d("TAG", DatabaseUtils.dumpCursorToString(cursor));
+
+                int columnIndex = cursor.getColumnIndex(projection[0]);
+                caminhoFoto = cursor.getString(columnIndex); // returns null
+                cursor.close();
+
+                exibeFoto();
+                */
+
+            // Desse jeito funciona, mas nao consigo salvar o caminho da foto
+
+            Uri uri = data.getData();
+
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                // Log.d(TAG, String.valueOf(bitmap));
+
+                campoFoto.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
         }
+
+
+    }
+
+    // By using this method get the Uri of Internal/External Storage for Media
+    private Uri getUri() {
+        String state = Environment.getExternalStorageState();
+        if(!state.equalsIgnoreCase(Environment.MEDIA_MOUNTED))
+            return MediaStore.Images.Media.INTERNAL_CONTENT_URI;
+
+        return MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
     }
 
     private void exibeFoto(){
