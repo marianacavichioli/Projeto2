a442cbdb1d641d8ada497cd711f14fe5d175f087
Renata <renatassmendes@hotmail.com>
2018-06-23 23:11:20 -0300

    Finalizado feedback

--- app/src/main/java/com/mobile2/projeto2/activities/feedback/FeedbackActivity.java
+++ app/src/main/java/com/mobile2/projeto2/activities/feedback/FeedbackActivity.java
@@ -36,7 +36,13 @@ public class FeedbackActivity extends AppCompatActivity{
         ButterKnife.bind(this);
 
         for(int i = 0; i< feedbacks.size(); i++){
-            stringFeedback += feedbacks.get(i).getType() + " " + feedbacks.get(i).getWord() + ": " + feedbacks.get(i).getMissCounter() + "\n";
+            if (feedbacks.get(i).getType().toString() == "IMAGE"){
+                stringFeedback += "Imagem ";
+            }
+            else{
+                stringFeedback += "Vídeo ";
+            }
+            stringFeedback += feedbacks.get(i).getWord() + ": " + feedbacks.get(i).getMissCounter() + "\n";
         }
         tentativas.setText(stringFeedback);
     }
